f474273c7ae8970693bfb3d5512b97f7c9a40cf4
firmanhoy <firmanhoys@gmail.com>
2023-02-01 20:09:43 +0700

    interface master sapi(edit,delete)

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,21 @@
-const btnEdit = document.getElementById('btnEdit');
-const parent = document.getElementById('parent');
-console.log(btnEdit);
-console.log(parent);
+$(document).ready(function () {
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+  /* When click show user */
+  $('body').on('click', '#btnEdit', function () {
+    var kodeSapi = $(this).data('id');
+    $.get('/cows/'+ kodeSapi +'/edit', function (data) {
+          console.log(data);
+        $('#jk').html(data.jenis_kelamin);
+        $('#umur').val(data.umur);
+        $('#bobot').val(data.bobot);
+        $('#tgl_lahir').val(data.tgl_lahir);
+        $('#edit-form').attr('action', '/cows/'+data.kode_sapi);
+    })
+  });
+
+});
